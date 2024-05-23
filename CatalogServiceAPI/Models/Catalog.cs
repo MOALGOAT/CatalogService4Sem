ae98ed8234e3d9b55d11c4c2281125cb5f2bc730
Rasmus Damgaard <rasmusdamgaard7@gmail.com>
2024-05-23 15:33:08 +0200

    fix af model

--- CatalogServiceAPI/Models/Catalog.cs
+++ CatalogServiceAPI/Models/Catalog.cs
@@ -2,24 +2,15 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace EnvironmentAPI.Models
 {
-    public class Catalog {
-
+    public class Catalog
+    {
         [BsonId]
-        public Guid _id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-
-        public string Category { get; set; }
-
-        public string Minimumprice { get; set; }
-
-        public string status { get; set; }
-
-        public string buyer { get; set; }
-
-        public string seller { get; set; }
-
-        public string logistic { get; set; }
+     public  Guid _id { get; set; }
+     public string title { get; set; }
+    public string description { get; set; }
+    public string category { get; set; }
+    public  float startingPrice { get; set; }
+    public  string status { get; set; }
 
     }
 }
